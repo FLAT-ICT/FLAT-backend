d61774743c8f452a7342061f9bbe0a453270b707
t4t5u0 <ymmtryk0902@gmail.com>
2021-10-02 15:45:53 +0900

    Add: User構造体周りを初期化

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
 use axum::{
     handler::{get, post},
-    http::StatusCode,
-    response::IntoResponse,
-    Json, Router,
+    Router,
 };
-use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+
+use tracing;
+use tracing_subscriber;
+
+mod user;
+use user::controller;
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    // tracing_subscriber::fmt::init();
+    // トレーサーを初期化
+    tracing_subscriber::fmt::init();
 
-    // build our application with a route
+    // ルーターを作成
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", post(controller::create_user));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
+    // バインドするアドレス
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // tracing::debug!("listening on {}", addr);
+
+    tracing::debug!("listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -32,33 +36,4 @@ async fn main() {
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> impl IntoResponse {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
---
+++ src/user/controller.rs
@@ -0,0 +1,20 @@
+use axum::{response::IntoResponse, Json};
+use hyper::StatusCode;
+
+use super::view::{self, User};
+
+pub async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<view::CreateUser>,
+) -> impl IntoResponse {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
