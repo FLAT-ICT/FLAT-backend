4d3502cdaea83ee1e1d60ecb5e79be35c9479a66
t4t5u0 <ymmtryk0902@gmail.com>
2021-10-13 00:47:42 +0900

    Update: up.sql をスキーマにあわせた

--- flat_backend/migrations/2021-10-04-101653_test1/up.sql
+++ flat_backend/migrations/2021-10-04-101653_test1/up.sql
@@ -1,5 +1,16 @@
 -- Your SQL goes here
 CREATE TABLE users (
     id INT PRIMARY KEY,
-    name TEXT
+    user_id TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    status INT,
+    beacon TEXT,
+    icon_path TEXT,
+    hashed_password TEXT
+);
+CREATE TABLE friends (
+    id INT PRIMARY KEY,
+    acctive TEXT NOT NULL,
+    pussive TEXT NOT NULl,
+    delete_flag BOOLEAN DEFAULT 'false'
 );
