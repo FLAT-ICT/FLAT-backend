de9b34adea1bfeb8afc12f2b7d96a1c05d41ee4c
t4t5u0 <ymmtryk0902@gmail.com>
2021-10-24 03:47:23 +0900

    Update: Test用のマルチステージビルドができた。Prodはまだだめ

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 
-echo $('ls')
-echo $('pwd')
+echo "$(ls scripts)"
+# echo $('pwd')
 # $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- mysql -udocker -p9Xu84SA1 -h mysql diesel_demo
 $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
 # diesel setup
 diesel migration run
-cargo run 
+# cargo run 
+flat_backend
--- scripts/start_test.sh
+++ scripts/start_test.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-
-echo $('ls')
-echo $('pwd')
-# $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- mysql -udocker -p9Xu84SA1 -h mysql diesel_demo
 $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
-# diesel setup
 diesel migration run
-cargo test
+cargo test --frozen
+# flat_backend
