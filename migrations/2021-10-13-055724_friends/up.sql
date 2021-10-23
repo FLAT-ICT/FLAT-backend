ef29e05a82efb28e3f4597177e07dcee859b7be8
t4t5u0 <ymmtryk0902@gmail.com>
2021-10-23 21:21:04 +0900

    Update: テーブル構造書き換えた。こっちのほうが正しいので

--- migrations/2021-10-13-055724_friends/up.sql
+++ migrations/2021-10-13-055724_friends/up.sql
@@ -1,8 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE friends (
-    id INT NOT NULL AUTO_INCREMENT,
     acctive TEXT NOT NULL,
     pussive TEXT NOT NULl,
     delete_flag BOOLEAN NOT NULL default 0,
-    PRIMARY KEY(id)
+    PRIMARY KEY(acctive(20), pussive(20))
 );
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,5 @@
 table! {
-    friends (id) {
-        id -> Integer,
+    friends (acctive, pussive) {
         acctive -> Text,
         pussive -> Text,
         delete_flag -> Bool,
@@ -9,7 +8,6 @@ table! {
 
 table! {
     users (user_id) {
-        id -> Integer,
         user_id -> Text,
         user_name -> Text,
         status -> Integer,
@@ -19,4 +17,7 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(friends, users,);
+allow_tables_to_appear_in_same_query!(
+    friends,
+    users,
+);
