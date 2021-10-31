e317b39dd5c5b76419f9a7e5555fa7a50c047004
t4t5u0 <ymmtryk0902@gmail.com>
2021-10-31 18:18:00 +0900

    Update: 間違ってビーこん消した

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 
 echo "$(ls scripts)"
+./import_csv.sh
 # echo $('pwd')
 # $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- mysql -udocker -p9Xu84SA1 -h mysql diesel_demo
 $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
