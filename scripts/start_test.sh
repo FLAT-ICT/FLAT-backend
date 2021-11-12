9e69a81d38fb5d4d5dab547f3c99e24406884729
t4t5u0 <ymmtryk0902@gmail.com>
2021-11-13 01:15:10 +0900

    Update: SSHで落ちてるっぽい

--- scripts/start_test.sh
+++ scripts/start_test.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# yes yes | ssh -T git@github.com -v
+ssh -oStrictHostKeyChecking=no -T git@github.com
+if [ $? -ne 0 ]; then
+    echo 'ssh接続失敗'
+    exit 1
+fi
 $('pwd')/scripts/import_csv.sh
-if [ $? -eq 1 ]; then
+if [ $? -ne 0 ]; then
     echo 'csvを取得できません'
     exit 1
 fi
@@ -9,15 +15,7 @@ $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
 diesel migration run
 flat_backend &
 cargo test
-if [ $? -eq 1 ]; then
-    echo 'csvを取得できません'
-    exit 1
-fi
-$('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
-diesel migration run
-flat_backend &
-cargo test
-if [ $? -eq 1 ]; then
+if [ $? -ne 0 ]; then
     echo 'testが落ちました'
     exit 1
 fi
