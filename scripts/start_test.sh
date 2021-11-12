a3aebcadb91d779fa3a02cb9cb736858c70f9516
t4t5u0 <ymmtryk0902@gmail.com>
2021-11-12 22:26:34 +0900

    Update: Test はちゃんと落ちるべき

--- scripts/start_test.sh
+++ scripts/start_test.sh
@@ -9,3 +9,15 @@ $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
 diesel migration run
 flat_backend &
 cargo test
+if [ $? -eq 1 ]; then
+    echo 'csvを取得できません'
+    exit 1
+fi
+$('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
+diesel migration run
+flat_backend &
+cargo test
+if [ $? -eq 1 ]; then
+    echo 'testが落ちました'
+    exit 1
+fi
