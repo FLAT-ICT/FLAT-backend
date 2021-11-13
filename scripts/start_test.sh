0252de02809a3e9b25d33d8f33b4fcb0b6a962ab
t4t5u0 <ymmtryk0902@gmail.com>
2021-11-13 18:10:59 +0900

    Update: デバッグのため、shを実行する場所をDockerfileからshにうつした

--- scripts/start_test.sh
+++ scripts/start_test.sh
@@ -6,11 +6,11 @@
 #     echo 'ssh接続失敗'
 #     exit 1
 # fi
-# $('pwd')/scripts/import_csv.sh
-# if [ $? -ne 0 ]; then
-#     echo 'csvを取得できません'
-#     exit 1
-# fi
+$('pwd')/scripts/import_csv.sh
+if [ $? -ne 0 ]; then
+    echo 'csvを取得できません'
+    exit 1
+fi
 $('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
 diesel migration run
 flat_backend &
