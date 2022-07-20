7b04393bb490fd4dadd9cabe51f67f8cc3a90531
t4t5u0 <ymmtryk0902@gmail.com>
2022-07-20 13:37:47 +0900

    Update: 移行完了．移行理由は，DieselがPG推奨なため

--- migrations/2021-10-04-101653_users/up.sql
+++ migrations/2021-10-04-101653_users/up.sql
@@ -1,14 +1,25 @@
 -- Your SQL goes here
+CREATE FUNCTION set_update_time() RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'UPDATE') THEN
+        NEW.updated_at = now();
+        RETURN NEW;
+    END IF;
+END;
+$$ language plpgsql;
+
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id SERIAL,
     name VARCHAR(10) NOT NULL UNIQUE,
     status INT NOT NULL DEFAULT 0,
     spot TEXT,
     icon_path TEXT NOT NULL,
-    salt BINARY(64) NOT NULL,
-    hash BINARY(64) NOT NULL,
+    salt BYTEA NOT NULL,
+    hash BYTEA NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     logged_in_at TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TRIGGER trg_updated_at BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE set_update_time();
--- scripts/start_test.sh
+++ scripts/start_test.sh
@@ -5,7 +5,7 @@ if [ $? -ne 0 ]; then
     echo 'csvを取得できません'
     # exit 1
 fi
-$('pwd')/scripts/wait-for-it.sh --timeout=90 --strict mysql:3306 -- diesel setup
+$('pwd')/scripts/wait-for-it.sh --timeout=90 --strict postgres:5432 -- diesel setup
 diesel migration run
 flat_backend &
 cargo test -- --test-threads=1
